8911b4f8070dc4a567a83f156603afee06ec7da9
Khalid <khaltahan@gmail.com>
2022-10-09 02:12:41 -0400

    tutorial 7

--- NodeJS/03-npm_modules/middleware/logEvents.js
+++ NodeJS/03-npm_modules/middleware/logEvents.js
@@ -14,19 +14,25 @@ const logEvents = async (message, logName) => {
   console.log(logItem);
   try {
     // If "logs" directory does not exist
-    if (!fs.existsSync(path.join(__dirname, "logs"))) {
+    if (!fs.existsSync(path.join(__dirname, "..", "logs"))) {
       // Make a directory called "logs"
-      await fsPromises.mkdir(path.join(__dirname, "logs"));
+      await fsPromises.mkdir(path.join(__dirname, "..", "logs"));
     }
     // Append logItem to eventLog.txt file in "logs" directory
-    await fsPromises.appendFile(path.join(__dirname, "logs", logName), logItem);
+    await fsPromises.appendFile(path.join(__dirname, "..", "logs", logName), logItem);
   } catch (err) {
     console.log(err);
   }
 };
 
+const logger = (req, res, next) => {
+  logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, "reqLog.txt");
+  console.log(`${req.method} ${req.path}`);
+  next();
+};
+
 // Export the logEvents function
-module.exports = logEvents;
+module.exports = { logger, logEvents };
 
 // console.log(format(new Date(), "yyyyMMdd\tHH:mm:ss"));
 
--- NodeJS/03-npm_modules/server.js
+++ NodeJS/03-npm_modules/server.js
@@ -1,8 +1,39 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-
+const cors = require("cors");
+const { logger, logEvents } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3500;
+
+// custom middleware logger
+app.use(logger);
+
+// Cross Origin Resource Sharing
+const whitelist = ["http://127.0.0.1:5500", "http://localhost:3500"];
+const corsOptions = {
+  origin: (origin, callback) => {
+    // If it's not one of the website, or if it's undefined
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+// built-in middleware to handle urlencoded data
+// in other words, form data:
+// "content-type: application/x-www-form-urlencoded"
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json
+app.use(express.json());
+
+// Sets files in that folder (images, css .etc) as public accessible by html
+app.use(express.static(path.join(__dirname, "/public")));
 
 // app.get("/", (req, res) => {
 //   res.send("Hello world!");
@@ -57,10 +88,21 @@ const three = (req, res) => {
 app.get("/chain(.html)?", [one, two, three]);
 
 // If nothing has been found
-app.get("/*", (re, res) => {
+app.all("*", (req, res) => {
   // We add the status(404) becuase if we don't, it will send a 200 status
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
+
+// Error handling is already done by Express
+// If we were to set error handling, we set it up at the end of the document as Express uses a waterfall approach
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
